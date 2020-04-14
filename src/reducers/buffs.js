db71321b63ae0e27e001136d5607ea8c3649df2a
Aleksandr Khristoforov <alexander.hrst@gmail.com>
2020-04-14 20:00:44 +0700

    Refactored buff data reducers

--- src/reducers/buffs.js
+++ src/reducers/buffs.js
@@ -17,29 +17,34 @@ const updateBuffs = (state, action) => {
     }
 
     case 'BUFF_DELETED': {
+      const data = state.buffs.data.filter((buff) => buff.code !== action.payload);
+
       return {
         ...state.buffs,
-        data: state.buffs.data.filter((buff) => buff.code !== action.payload)
+        data
       }
     }
 
     case 'CURRENT_BUFF_SAVED': {
       const updatingIndex = state.buffs.data.findIndex((buff) => buff.code === action.payload.code);
+      const data = [
+        ...state.buffs.data.slice(0, updatingIndex),
+        action.payload,
+        ...state.buffs.data.slice(updatingIndex + 1)
+      ];
 
       return {
         ...state.buffs,
-        data: [
-          ...state.buffs.data.slice(0, updatingIndex),
-          action.payload,
-          ...state.buffs.data.slice(updatingIndex + 1)
-        ]
+        data
       }
     }
 
     case 'CURRENT_BUFF_CREATED': {
+      const data = [...state.buffs.data, action.payload];
+
       return {
         ...state.buffs,
-        data: [...state.buffs.data, action.payload],
+        data
       }
     }
 
